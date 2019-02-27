95a163ee42753fa938cb1dfd2c21675e3a3a80cc
root <root@nthimmappa-linux-01.nvidia.com>
2019-02-27 08:48:54 +0000

    changed port to 80

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
